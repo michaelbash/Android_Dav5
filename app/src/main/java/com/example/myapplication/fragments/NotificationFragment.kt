515ce3ff6dc1eeac6384e06d54bbe55bec92934a
unknown <misho.bashe12@yahoo.com>
2021-05-09 18:14:05 +0400

    first commit

---
+++ app/src/main/java/com/example/myapplication/fragments/NotificationFragment.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+/*
+ *  Created by Nikoloz Katsitadze on 5/4/21
+ */
+
+class NotificationFragment : Fragment(R.layout.fragment_notification) {
+}
